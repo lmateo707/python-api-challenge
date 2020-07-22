b26c166c0892c7186a4808c0b41d1209c15968ab
Lorenz Mateo <mateo707@hawaii.edu>
2020-07-22 14:48:38 -0700

    revised

--- VacationPy/api_keys.py
+++ VacationPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "815723b5303463214a58c63ab1eefd9f"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyBp_3CAIZHcSdU5UGRcfvK5g28G54dlISg"
